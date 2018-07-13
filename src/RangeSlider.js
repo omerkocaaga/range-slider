4bface8299f3b6c3d741f58eee9f812f2103a47b
Ömer Faruk Kocaağa <omerfaruk.kocaaga@solvoyo.local>
2018-07-13 16:29:11 +0300

    initial

---
+++ src/RangeSlider.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './RangeSlider.scss'
+import './App.css'
+
+class RangeSlider extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      slots: 20,
+      default: 0
+    }
+    this.updateSlider = this.updateSlider.bind(this)
+    this.myRef = React.createRef()
+  }
+
+  updateSlider () {
+    const domElement = ReactDOM.findDOMNode(this.refs.rangeBar)
+    console.log(domElement)
+  }
+  render () {
+    return (
+      <div ref='container' className='slider-container'>
+        <div onClick={this.updateSlider} className='rangeBar' />
+        <div ref='pin' className='dot' />
+      </div>
+    )
+  }
+}
+
+export default RangeSlider
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import RangeSlider from './RangeSlider'
+import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(<RangeSlider />, document.getElementById('root'))
+registerServiceWorker()
